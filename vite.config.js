fdc825ddccd9255eba3246ec460738ad27cd0dff
Winter <zpl200522@gmail.com>
2025-08-29 01:12:47 +0800

    第3次更改

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/Website1/',
+  base: '/week34-work/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
